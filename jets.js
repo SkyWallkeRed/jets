f876b64e43e17c0773ec4b9de30f6ac208da0990
chaimochs <chaimochs@gmail.com>
2018-05-13 15:00:19 +0300

    added fly method

---
+++ jets.js
@@ -0,0 +1,11 @@
+class Jet {
+    constructor(color, size){
+      this.color = color;
+      this.size = size;
+    }
+    fly () {
+      console.log('whoosh!');
+    }
+  }
+  
+  module.exports = Jet;
